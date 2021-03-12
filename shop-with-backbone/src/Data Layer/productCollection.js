2e74b2964b4f255b5b9132b2baf73a8b6f4f0ede
Nowfel2k <muhammednavfal1@gmail.com>
2021-03-12 12:17:55 +0530

    simplified shopping app

--- shop-with-backbone/src/Data Layer/productCollection.js
+++ shop-with-backbone/src/Data Layer/productCollection.js
@@ -1,5 +1,6 @@
 import { Collection } from 'backbone'
 import ProductModel from './productModel';
+import { list } from './productsData';
 
 
 let ProductCollection = Collection.extend({
@@ -14,5 +15,15 @@ let ProductCollection = Collection.extend({
 
 let collection = new ProductCollection()
 let cart = new ProductCollection()
+
+for (let i = 0; i < list.length; i++) {
+    let p = new ProductModel({
+        id: list[i].id,
+        name: list[i].name,
+        image: list[i].image,
+        price: list[i].price,
+    })
+    collection.add(p)
+}
 
 export { collection, cart }
---
+++ shop-with-backbone/src/Data Layer/productsData.js
@@ -0,0 +1,55 @@
+
+// raw data 
+let list = [
+    {
+        id: 1,
+        name: 'milk',
+        price: 23,
+        image: 'https://www.gannett-cdn.com/-mm-/4e57f417ab56d8147a59ed2deb8f3a4b0b446da3/c=0-108-2120-1306/local/-/media/2017/02/28/USATODAY/USATODAY/636238697589955973-GettyImages-479352566.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'rice',
+        price: 50,
+        image: 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/how-to-cook-rice.jpg'
+    },
+
+    {
+        id: 3,
+        name: 'flour',
+        price: 60,
+        image: 'https://www.bakingbusiness.com/ext/resources/2020/5/WholeWheatFlour_Lead.jpg?t=1588771711&width=1080'
+    },
+
+    {
+        id: 4,
+        name: 'chocolate',
+        price: 60,
+        image: 'https://www.history.com/.image/t_share/MTc4MjEwMDg3NDg2MTA1MTk3/chocolate-gettyimages-473741340.jpg'
+    },
+
+    {
+        id: 5,
+        name: 'soap',
+        price: 30,
+        image: 'https://www.twincraft.com/hubfs/bare-bar-soap-array.jpg'
+    },
+
+    {
+        id: 6,
+        name: 'tea',
+        price: 50,
+        image: 'https://images.financialexpress.com/2019/12/tata-Tea.jpg?w=1200&h=800&imflag=true'
+    },
+
+
+    {
+        id: 7,
+        name: 'coffee',
+        price: 20,
+        image: 'https://live-production.wcms.abc-cdn.net.au/925f5a620821411566769c52377aec07'
+    },
+]
+
+export { list }
